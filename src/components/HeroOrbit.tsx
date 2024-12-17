5d83fc13680d6fa41751f002f689edc9ae61447c
AndrMaulana <andryymaulana2000@gmail.com>
2024-12-17 23:24:54 +0700

    hero orbit inline position

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -7,13 +7,15 @@ export const HeroOrbit = ({
 }: PropsWithChildren<{size: number; rotation: number;}>) => {
     return (
         <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-        <div className="border border-red-500"
+        <div className="flex items-start justify-start"
         style={{ 
             transform: `rotate(${rotation}deg)`,
             height: `${size}px`,
             width: `${size}px`
          }}>
-          <div className="border border-red-500 inline-flex">
+          <div className="inline-flex" style={{ 
+            transform: `rotate(${rotation * -1}deg)`
+           }}>
           {children}
             </div>
           </div>
