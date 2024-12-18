c633505c778ffd75e86ee2e11c866d7ec204378d
AndrMaulana <andryymaulana2000@gmail.com>
2024-12-18 18:56:20 +0700

    resize padding button for desktop layout

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -64,7 +64,7 @@ export const ProjectsSection = () => {
                 backgroundImage: `url(${grainImage.src})`
                }}></div>
                <div className="lg:grid lg:grid-cols-2 lg:gap-16">
-                <div>
+                <div className="lg:pb-16">
                 <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
                 <span>{project.company}</span>
                 <span>&bull;</span>
