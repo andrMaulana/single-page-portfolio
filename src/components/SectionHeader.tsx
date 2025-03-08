b36e6c85465d773c2b926e6f0f44001622fb34ff
AndrMaulana <andryymaulana2000@gmail.com>
2025-03-08 21:29:14 +0700

    feat(testimonial): ✨ add data feature for section header

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -1,20 +1,21 @@
-export const SectionHeader = () => {
+export const SectionHeader = ({
+  title,
+  eyebrow,
+  description,
+}: {
+    title: string;
+    eyebrow: string;
+    description: string;
+  }) => {
   return (
     <>
         <div className="flex justify-center">
-        <div className="flex justify-center">
         <p className="uppercase font-semibold tracking-widest bg-gradient-to-t from-emerald-300 to-sky-400 text-center bg-clip-text text-transparent">
-          Real-world Results
+          {eyebrow}
         </p>
         </div>
-        <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">Featured Projects</h2>
-        <p className="text-center md:text-lg lg:text-xl text-white/60 mt-4 max-w-md mx-auto">See how I transformed concept into engaging digital experiences.</p>
-        <p className="uppercase font-semibold tracking-widest bg-gradient-to-t from-emerald-300 to-sky-400 text-center bg-clip-text text-transparent">
-          Real-world Results
-        </p>
-        </div>
-        <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">Featured Projects</h2>
-        <p className="text-center md:text-lg lg:text-xl text-white/60 mt-4 max-w-md mx-auto">See how I transformed concept into engaging digital experiences.</p>
-    </>
-  )
-}
+        <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">{title}</h2>
+        <p className="text-center md:text-lg lg:text-xl text-white/60 mt-4 max-w-md mx-auto">{description}</p>
+  </>
+  );
+};
