c7a44488b29f022d11ca5e64c5064ba59a4472ad
AndrMaulana <andryymaulana2000@gmail.com>
2025-03-08 21:21:14 +0700

    feat(testimonial): ✨ create section header component

---
+++ src/components/SectionHeader.tsx
@@ -0,0 +1,20 @@
+export const SectionHeader = () => {
+  return (
+    <>
+        <div className="flex justify-center">
+        <div className="flex justify-center">
+        <p className="uppercase font-semibold tracking-widest bg-gradient-to-t from-emerald-300 to-sky-400 text-center bg-clip-text text-transparent">
+          Real-world Results
+        </p>
+        </div>
+        <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">Featured Projects</h2>
+        <p className="text-center md:text-lg lg:text-xl text-white/60 mt-4 max-w-md mx-auto">See how I transformed concept into engaging digital experiences.</p>
+        <p className="uppercase font-semibold tracking-widest bg-gradient-to-t from-emerald-300 to-sky-400 text-center bg-clip-text text-transparent">
+          Real-world Results
+        </p>
+        </div>
+        <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">Featured Projects</h2>
+        <p className="text-center md:text-lg lg:text-xl text-white/60 mt-4 max-w-md mx-auto">See how I transformed concept into engaging digital experiences.</p>
+    </>
+  )
+}
