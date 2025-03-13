dc5c0d660a11508e2ae29a866c6faf54c55ef910
AndrMaulana <andryymaulana2000@gmail.com>
2025-03-13 22:05:30 +0700

    Style: add -z-10

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -5,7 +5,7 @@ import memojiAvatar4 from "@/assets/images/memoji-avatar-4.png";
 import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
 import { SectionHeader } from "@/components/SectionHeader";
 import Image from "next/image";
-import grainImage from '@assets/images/grain.jpg';
+import grainImage from '@/assets/images/grain.jpg';
 
 const testimonials = [
   {
@@ -48,7 +48,8 @@ export const TestimonialsSection = () => {
 "/>
     <div>
       {testimonials.map(testimonial => (
-      <div key={testimonial.name} className="bg-gray-800 rounded-3xl p-6">
+      <div key={testimonial.name} className="bg-gray-800 rounded-3xl p-6 relative overflow-hidden z-0">
+          <div className="absolute inset-0 opacity-5 -z-10" style={{backgroundImage: `url(${grainImage.src})`,}}></div>
         <Image src={testimonial.avatar} alt={testimonial.name} />
           <div>{testimonial.name}</div>
           <div>{testimonial.position}</div>
